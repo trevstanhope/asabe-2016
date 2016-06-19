032ed3eef4b0ae2be567605b3416ddf5ef256e6c
Trevor Stanhope <tpstanhope@gmail.com>
2016-06-19 22:23:50 +0000

    added installer

---
+++ conf/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git config --global user.name "Trevor Stanhope"
+git config --global user.email "tpstanhope@gmail.com"
+apt-get update
+apt-get upgrade
+apt-get install build-essential cmake -y -qq
+apt-get install python-dev python-setuptools python-pip -y -qq
+apt-get install python-opencv python-zmq python-serial python-cherrypy3 -y -qq
+apt-get install arduino arduino-mk -y -qq
+apt-get install python-numpy -y -qq
+
+# Arduino Libraries
+cp -r libraties/* /usr/share/arduino/libraries
