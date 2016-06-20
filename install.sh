547fe5e388188b33f6fc9e261cb9fd93d02422b2
Trevor Stanhope <tpstanhope@gmail.com>
2016-06-20 16:45:05 +0000

    fixed install for OpenCV

--- install.sh
+++ install.sh
@@ -5,9 +5,20 @@ apt-get update
 apt-get upgrade
 apt-get install build-essential cmake -y -qq
 apt-get install python-dev python-setuptools python-pip -y -qq
-apt-get install python-opencv python-zmq python-serial python-cherrypy3 -y -qq
+apt-get install python-zmq python-serial python-cherrypy3 -y -qq
 apt-get install arduino arduino-mk -y -qq
-apt-get install python-numpy -y -qq
+apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev -y -qq
+apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y -qq
+apt-get install ipython -y -qq
 
 # Arduino Libraries
 cp -r libraries/* /usr/share/arduino/libraries
+
+# OpenCV
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+mkdir release
+cd release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make
+sudo make install
