6238f926854b296cf457f803d8d810a59be64ef9
trevstanhope <tpstanhope@gmail.com>
2016-07-12 16:02:18 -0400

    better tester

--- test/execute_action.py
+++ test/execute_action.py
@@ -1,7 +1,7 @@
 import serial, sys
  
 for dev in ['/dev/ttyACM0', '/dev/ttyUSB0']:
-    for i in range(attempts):
+    for i in range(5):
         try:
             s = Serial(dev + str(i), 9600, timeout=2)
             time.sleep(wait)
