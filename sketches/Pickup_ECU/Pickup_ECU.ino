5fc0e7936a979040e2e4844e72e9a8d270a00ae3
trevstanhope <tpstanhope@gmail.com>
2016-07-12 18:37:12 -0400

    T dir

--- sketches/Pickup_ECU/Pickup_ECU.ino
+++ sketches/Pickup_ECU/Pickup_ECU.ino
@@ -353,19 +353,19 @@ int transfer(void) {
   while (true)  {
     x = line_detect();
     if (x == -2) {
-      set_wheel_servos(SERVO_MEDIUM, SERVO_MEDIUM, SERVO_MEDIUM, SERVO_MEDIUM);
+      set_wheel_servos(-SERVO_MEDIUM, -SERVO_MEDIUM, -SERVO_MEDIUM, -SERVO_MEDIUM);
     }
     else if (x == -1) {
-      set_wheel_servos(-SERVO_MEDIUM, SERVO_FAST, -SERVO_MEDIUM, SERVO_FAST);
+      set_wheel_servos(-SERVO_FAST, SERVO_MEDIUM, -SERVO_FAST, SERVO_MEDIUM);
     }
     else if (x == 0) {
       set_wheel_servos(-SERVO_MEDIUM, SERVO_MEDIUM, -SERVO_MEDIUM, SERVO_MEDIUM);
     }
     else if (x == 1) {
-      set_wheel_servos(-SERVO_FAST, SERVO_MEDIUM, -SERVO_FAST, SERVO_MEDIUM);
+      set_wheel_servos(-SERVO_MEDIUM, SERVO_FAST, -SERVO_MEDIUM, SERVO_FAST);
     }
     else if (x == 2) {
-      set_wheel_servos(-SERVO_MEDIUM, -SERVO_MEDIUM, -SERVO_MEDIUM, -SERVO_MEDIUM);
+      set_wheel_servos(SERVO_MEDIUM, SERVO_MEDIUM, SERVO_MEDIUM, SERVO_MEDIUM);
     }
     else if (x == 255) {
       break;
